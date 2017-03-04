92316a81c305e14692ab1cffa31a40069e39c5c3
Caleb Zulawski <caleb.zulawski@gmail.com>
2017-03-04 17:44:44 -0500

    Add low resolution image generation

---
+++ model.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+
+def gaussian_2d(size, sigma):
+    """
+    Returns a Gaussian kernel of shape `size` x `size` with standard deviation `sigma`.
+    """
+
+    w = (size - 1.)/2.
+    y,x = np.ogrid[-w:w+1, -w:w+1]
+    h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+    h[h < np.finfo(h.dtype).eps*h.max()] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
+
+def gaussian_kernel(channels, size, sigma):
+    """
+    Returns a Gaussian kernel of size [size, size, channels, 1] with standard deviation `sigma`.
+    """
+
+    kernel = gaussian_2d(size, sigma)
+    kernel = np.repeat(kernel[:, :, np.newaxis], channels, axis=2)
+    return np.expand_dims(kernel, 3)
+
+def produce_low_resolution(input, k=3, blur_size=3, blur_sigma=0.5):
+    """
+    Produces a batch of low resolution images from the high resolution images `input`.
+    The images are produced by applying a Gaussian blur with kernel size `blur_size` x `blur_size` and standard deviation `blur_sigma`,
+    downsampling by `k`, and applying bicubic interpolation back to the size of the input batch.
+    """
+
+    n_channels = input.get_shape().as_list()[3];
+
+    # Apply Gaussian blur
+    kernel = gaussian_kernel(n_channels, blur_size, blur_sigma)
+    lr = tf.nn.depthwise_conv2d_native(input, kernel, [1, 1, 1, 1], 'VALID')
+
+    # Downsample the image
+    lr = tf.nn.depthwise_conv2d_native(lr, tf.ones([1, 1, n_channels, 1]), [1, k, k, 1], 'VALID')
+
+    # Apply bicubic interpolation
+    lr = tf.image.resize_bicubic(lr, input.get_shape().as_list()[1:3])
