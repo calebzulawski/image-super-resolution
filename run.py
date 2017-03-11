fd46cba46554318dbb2e8c81264f616be8348ad8
Kelvin Lin <lin.kelvin11@gmail.com>
2017-03-11 15:26:25 -0500

    hardcoded input image

--- run.py
+++ run.py
@@ -37,6 +37,10 @@ if __name__ == '__main__':
             v.stop_queue_runners()
             f.stop_queue_runners()
     elif args.mode == 'generate':
+        if args.input is None:
+            print("must provide an input file in generate mode")
+            sys.exit(1)
+        from files import FileReader
         m = Model(is_training=False)
         m.build_model()
         with tf.Session() as sess:
@@ -48,7 +52,39 @@ if __name__ == '__main__':
                 print('Could not load model file: {}!'.format(args.model))
                 sys.exit(1)
             # Generate new images here
+            filename_queue = tf.train.string_input_producer(['./input/input.JPEG'])
+            image_reader = tf.WholeFileReader()
+            _, image_file = image_reader.read(filename_queue)
+            image = tf.image.decode_jpeg(image_file, channels=3)
+            # Crop the image to the desired size
+            crop_shape = tf.concat([(128,128), [3]], 0)
+            cropped = tf.random_crop(image, crop_shape)
+            cropped = tf.cast(cropped, tf.float32)/255.
+
+            batch = tf.train.batch([cropped], batch_size=1)
+
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
+
+            output = sess.run(m.output, feed_dict={m.input: batch.eval()})
+            
+            coord.request_stop()
+            coord.join(threads)
             # Calculate PSNR gain for each one
+            print('encoding jpegs')
+            out_shape = output.shape
+            if len(out_shape) > 3:
+                out_shape = out_shape[1:]
+            print(out_shape)
+
+            output = output.reshape(out_shape)
+            
+            images_out = tf.image.encode_jpeg(output,name="output")
+            print('writing to file')
+            fh = open("./outputs/output.jpeg", "wb+")
+            fh.write(images_out.eval())
+            fh.close()
+            print('file closed')
     else:
         print('Invalid "mode": {}!'.format(args.mode))
     sys.exit(0)
